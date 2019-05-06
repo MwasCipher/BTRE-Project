7cfb2d136c3998764c41bc50fe73d971c0648d0b
MwasCipher <mwangicollinswaweru@gmail.com>
2019-05-06 08:31:13 +0300

    First Commit

--- btre/settings.py
+++ btre/settings.py
@@ -66,8 +66,11 @@ WSGI_APPLICATION = 'btre.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'btredb',
+        'USER': 'postgres',
+        'PASSWORD': '_Cipher96',
+        'HOST': 'localhost'
     }
 }
 
@@ -113,3 +116,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'btre/static')
 ]
+
+#Media Folder Settings
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
