4af67f0ed91f4c715fce057e1bdb5a77a533e2cb
MwasCipher <mwangicollinswaweru@gmail.com>
2020-02-25 23:38:02 +0300

    Static Root

--- btre/settings.py
+++ btre/settings.py
@@ -114,7 +114,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'btre/static')
@@ -124,6 +124,7 @@ STATICFILES_DIRS = [
 # Media Folder Settings
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 MEDIA_URL = '/media/'
 
