1652b0cf96ec727a55a9db553925932b631ca776
MwasCipher <mwangicollinswaweru@gmail.com>
2020-02-27 13:18:45 +0300

    Final Commit

--- btre/settings.py
+++ btre/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '9khxq0!zanhrc626ryvboz&md5)+gxv*7t9%tx**md6&_no%@!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
